7af99b07daa3f66166c0305997963977e13b2f21
Anders Høgden <andeshog@stud.ntnu.no>
2025-01-19 11:50:53 +0100

    feat: change name of launch argument from scenario_desc to task, and give choices

--- stonefish_sim/launch/simulation.launch.py
+++ stonefish_sim/launch/simulation.launch.py
@@ -1,20 +1,19 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
     # Get the directories of the involved packages
     vortex_stonefish_sim_dir = get_package_share_directory('stonefish_sim')
     stonefish_ros2_dir = get_package_share_directory('stonefish_ros2')
 
-    # Set default paths for simulation_data and scenario_desc
     simulation_data_default = PathJoinSubstitution([vortex_stonefish_sim_dir, 'data'])
     scenario_desc_default = PathJoinSubstitution([vortex_stonefish_sim_dir, 'scenarios'])
 
-    # Declare arguments
     simulation_data_arg = DeclareLaunchArgument(
         'simulation_data',
         default_value=simulation_data_default,
@@ -22,9 +21,10 @@ def generate_launch_description():
     )
 
     scenario_desc_arg = DeclareLaunchArgument(
-        'scenario_desc',
+        'task',
         default_value=PathJoinSubstitution([scenario_desc_default, 'demo_world.scn']),
-        description='Path to the scenario file'
+        description='Path to the scenario file',
+        choices=['docking.scn', 'structure.scn', 'demo_world.scn']
     )
 
     window_res_x_arg = DeclareLaunchArgument(
@@ -38,13 +38,13 @@ def generate_launch_description():
         default_value='1340',
         description='Window resolution height'
     )
-
-    # Logic to prepend the scenarios directory if only a filename is provided
+    
     scenario_desc_resolved = PathJoinSubstitution([
-        vortex_stonefish_sim_dir, 'scenarios', LaunchConfiguration('scenario_desc')
+        vortex_stonefish_sim_dir, 
+        'scenarios', 
+        LaunchConfiguration('task')
     ])
 
-    # Include the original launch file from stonefish_ros2 package
     include_stonefish_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([stonefish_ros2_dir, '/launch/stonefish_simulator.launch.py']),
         launch_arguments={
