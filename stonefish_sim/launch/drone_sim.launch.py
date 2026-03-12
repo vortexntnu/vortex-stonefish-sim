a355264ac6a4d8ff2a16409c91b5be8f22f31b6e
Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>
2026-03-12 18:02:50 +0100

    drone agnostic interface/sim launch file

---
+++ stonefish_sim/launch/drone_sim.launch.py
@@ -0,0 +1,96 @@
+from os import path
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchContext, LaunchDescription
+from launch.actions import (
+    IncludeLaunchDescription,
+    OpaqueFunction,
+    SetEnvironmentVariable,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from auv_setup.launch_arg_common import (
+    declare_drone_and_namespace_args,
+    resolve_drone_and_namespace,
+)
+
+
+def launch_setup(context: LaunchContext, *args, **kwargs):
+    drone, namespace = resolve_drone_and_namespace(context)
+
+    common_launch_args = {
+        "drone": drone,
+        "namespace": namespace,
+    }.items()
+
+    thrust_allocator_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            path.join(
+                get_package_share_directory("thrust_allocator_auv"),
+                "launch",
+                "thrust_allocator_auv.launch.py",
+            )
+        ),
+        launch_arguments=common_launch_args,
+    )
+
+    joystick_interface_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            path.join(
+                get_package_share_directory("joystick_interface_auv"),
+                "launch",
+                "joystick_interface_auv.launch.py",
+            )
+        ),
+        launch_arguments=common_launch_args,
+    )
+
+    operation_mode_manager_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            path.join(
+                get_package_share_directory("operation_mode_manager"),
+                "launch",
+                "operation_mode_manager.launch.py",
+            )
+        ),
+        launch_arguments=common_launch_args,
+    )
+
+    stonefish_sim_interface_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            path.join(
+                get_package_share_directory("stonefish_sim_interface"),
+                "launch",
+                "stonefish_sim_interface.launch.py",
+            )
+        ),
+        launch_arguments=common_launch_args,
+    )
+
+    return [
+        thrust_allocator_launch,
+        joystick_interface_launch,
+        operation_mode_manager_launch,
+        stonefish_sim_interface_launch,
+    ]
+
+
+def generate_launch_description():
+    set_env_var = SetEnvironmentVariable(
+        name="ROSCONSOLE_FORMAT",
+        value="[${severity}] [${time}] [${node}]: ${message}",
+    )
+
+    set_warn_color = SetEnvironmentVariable(
+        name="RCUTILS_COLORIZED_OUTPUT",
+        value="1",
+    )
+
+    return LaunchDescription(
+        declare_drone_and_namespace_args()
+        + [
+            set_env_var,
+            set_warn_color,
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
